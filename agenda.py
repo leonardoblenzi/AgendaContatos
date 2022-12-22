f362ec0081910e6a3539b072cb6ba080314c5421
Leonardo <leonardoblenzi@hotmail.com>
2022-12-22 09:03:36 -0300

    v1.0

---
+++ agenda.py
@@ -0,0 +1,20 @@
+#Estrutura de dados em dicionario
+#Criando dicionario dentro de dicionario{} para estruturar os valores
+agenda = {
+    'leo': {
+        'celular': '449987-5210',
+        'email': 'leonardo@email.com',
+        'endereco': 'Rua X'
+    },
+    'ana': {
+        'celular': '449956-4520',
+        'email': 'ana@email.com',
+        'endereco': 'Rua Y'
+    },
+    'joao': {
+        'celular': '449958-6310',
+        'email':'joao@email.com',
+        'endereco':'Rua Z'
+    }
+}
+
